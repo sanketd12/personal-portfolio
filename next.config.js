12e22f10eae1508c2751953b9936f2d175833a7b
Sanket <sanketdoddabendigere@gmail.com>
2025-05-20 21:15:30 -0400

    add basePath/assetPrefix for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/personal-portfolio',
+    assetPrefix: '/personal-portfolio/',
+    trailingSlash: true,
     images: {
       domains: ['assets.aceternity.com'],
+      unoptimized: true,
     },
   }
   
